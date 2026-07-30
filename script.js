b038c15abdcfaf932f327c0db9bb969c112e92cc
razveranm-dot <sales.razvera.nm@gmail.com>
2026-07-30 10:48:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("✅ script.js loaded");
 
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwcEug4GMj6JfC-PNrzNnry7yZw3sjFOfONK6cwCVGpdBXsH8pBvbH4SpyMGfESREslVA/exec"; // paste your /exec URL here
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzUVV_44MfJs4vbOzNqQPsIP99xx3P-T2B2VOuLGqQlGvZ8r-ukOJFyxYiKfRtHfflDZg/exec"; // paste your /exec URL here
 
 async function generateCode(type) {
   // Get client code (default RSS if empty)
